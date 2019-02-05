1826b5f18882fb86eaa3af822d20383da3c0192a
Mingming Li <mingming@mingmings-mbp.mobile.asu.edu>
2019-02-05 09:50:22 -0700

    2/5/2019

--- JupiterLab/input.py
+++ JupiterLab/input.py
@@ -1,2 +1,11 @@
 a=input("input something here:")
 print(a)
+# Calculates time, gallons of gas used, and cost of gasoline for a trip
+distance = input("Input distance of trip in miles: ")
+distance = float(distance)
+mpg = 30.
+speed = 60.
+costPerGallon = 4.10
+time = distance / speed
+gallons = distance / mpg
+cost = gallons * costPerGallon
